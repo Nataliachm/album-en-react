e8fd5181398f74c4dfcb4b56a8c7a1f7860daf58
NataliaChavarria <nataliachm97@gmail.com>
2023-06-29 10:19:33 -0500

    Create song card

--- album-en-react/src/App.jsx
+++ album-en-react/src/App.jsx
@@ -1,11 +1,15 @@
-import { useState } from 'react'
+// import { useState } from 'react'
 import './App.css'
+import Song from './assets/components/song'
 
 function App() {
 
 
   return (
     <>
+      <div className='container__albums' >
+        <Song />
+      </div>
 
     </>
   )
